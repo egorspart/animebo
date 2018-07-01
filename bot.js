a138f14bd65e6b08a19fd962901636cfe8667e0a
egorspart <40728458+egorspart@users.noreply.github.com>
2018-07-01 11:10:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,12 +9,9 @@ client.on('message', (message) =>{
     }
     });
     
-    client.on('message', (message) =>{
-    if(message.content == "бот")
-    {
-    message.channel.send("я тут");
-            
-    }
+  client.on('ready', () => {
+        console.log('I am ready!');
+        client.user.setGame('Anime.ru');
     });
     
     client.login(process.env.BOT_TOKEN);
