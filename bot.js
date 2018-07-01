e0f90012e210129ec01b69e1191287dfddeaad07
egorspart <40728458+egorspart@users.noreply.github.com>
2018-07-01 11:44:20 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,14 @@ const client = new Discord.Client();
 client.on('message', (message) =>{
     if(message.content == "бот")
     {
-    message.channel.send("я тут");
+    message.channel.send("Да?");
             
     }
     });
     
   client.on('ready', () => {
         console.log('I am ready!');
-        client.user.setGame('Anime.ru');
+        client.user.setActivity('Anime.ru');
     });
     
     client.login(process.env.BOT_TOKEN);
